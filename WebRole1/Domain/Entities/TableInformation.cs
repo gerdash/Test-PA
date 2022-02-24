490d937b7bb0ddec480ab286f5b3866a2405db45
GSADURSK <gerda.sadurska@volvocars.com>
2022-02-24 10:12:33 +0200

    Initial Migration WIP

---
+++ WebRole1/Domain/Entities/TableInformation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebRole1.Domain.Entities
+{
+    public class TableInformation
+    {
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string Currency { get; set; }
+        public string Language { get; set; }
+        public int NumberOfUsers { get; set; }
+        public string SalesModel { get; set; }
+
+    }
+}
---
+++ WebRole1/Models/MarketViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace WebRole1.Models
+{
+    public class MarketViewModel
+    {
+        public string Country { get; set; }
+        public string Currency { get; set; }
+        public string Language { get; set; }
+        public int NumberOfUsers { get; set; }
+        public string SalesModel { get; set; }
+    }
+}
